db779934a084821acbc080ebffca16890697086e
lepoSs <7715423+StefPoss@users.noreply.github.com>
2025-05-09 21:56:53 +0200

    xème commit !

--- api/envoyer.js
+++ api/envoyer.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ input: message })
+      body: JSON.stringify({ message })
     })
 
     const text = await response.text()
