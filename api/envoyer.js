7bac8d3de37cdc841508ea230b55a965a4b3be2a
lepoSs <7715423+StefPoss@users.noreply.github.com>
2025-05-09 22:08:00 +0200

    Update envoyer.js check d'url

--- api/envoyer.js
+++ api/envoyer.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
   const { message } = req.body
 
-  const zapierUrl = "https://mcp.zapier.com/api/mcp/s/NjZkODBiNTMtMGQyOS00OTgyLWJmZGItMmRmY2YwOGU2ZDQyOjRhOTBkNWE1LWIwNWEtNGRhNy05MzUxLTQxNDA5MWFmZmRiMQ=="
+  const zapierUrl = "https://mcp.zapier.com/api/mcp/s/NjZkODBiNTMtMGQyOS00OTgyLWJmZGItMmRmY2YwOGU2ZDQyOjRhOTBkNWE1LWIwNWEtNGRhNy05MzUxLTQxNDA5MWFmZmRiMQ==/sse"
 
   try {
     const response = await fetch(zapierUrl, {
